3e1cea46ae7a6e1c9e7be381881cb8d7c230ce37
Jean Stebens <jstebens@cpan.org>
2013-10-02 12:09:52 +0000

    Import of JSTEBENS/Whisper-1.023 from CPAN.
    gitpan-cpan-distribution: Whisper
    gitpan-cpan-version:      1.023
    gitpan-cpan-path:         JSTEBENS/Whisper-1.023.tar.gz
    gitpan-cpan-author:       JSTEBENS
    gitpan-cpan-maturity:     released

--- lib/Whisper.pm
+++ lib/Whisper.pm
@@ -15,11 +15,13 @@
 
 package Whisper;
 {
-  $Whisper::VERSION = '1.013';
+  $Whisper::VERSION = '1.023';
 }
 
 use strict;
 use warnings;
+
+use POSIX;
 
 our $VERSION;
 
@@ -117,7 +119,7 @@ sub wsp_info {
 }
 
 sub wsp_fetch {
-	my ($dbfile, $from, $until) = @_;
+	my ($dbfile, $from, $until, $do_tuples, $date_format) = @_;
 
 	die("You need to specify a wsp file\n") unless $dbfile;
 	
@@ -220,6 +222,23 @@ sub wsp_fetch {
 		}
 		$current_interval += $step;
 		$index++;
+	}
+
+	# Generate datetime,data tuples
+	if( $do_tuples ) {
+
+		my $current = $from_interval;
+		while( my ($i, $val) = each @$values ) {
+
+			my $timestamp = $current;
+			# Format the datetime field if wanted
+			if( $date_format ) {
+				$timestamp = POSIX::strftime($date_format, localtime($current));
+			}
+
+			$values->[$i] = [ $timestamp, $val ];
+			$current += $step;
+		}
 	}
 
 	close($file);
